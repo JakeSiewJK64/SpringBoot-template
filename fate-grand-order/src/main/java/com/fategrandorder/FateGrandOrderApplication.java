e41eefe8e58b6acf12c35ce9c97df070b7bba689
Siew Joe Kane <joekanesiew@outlook.com>
2021-07-08 15:42:49 +0800

    added base java application

---
+++ fate-grand-order/src/main/java/com/fategrandorder/FateGrandOrderApplication.java
@@ -0,0 +1,13 @@
+package com.fategrandorder.fategrandorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FateGrandOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FateGrandOrderApplication.class, args);
+	}
+
+}
---
+++ fate-grand-order/src/test/java/com/fategrandorder/fategrandorder/FateGrandOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fategrandorder.fategrandorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FateGrandOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
